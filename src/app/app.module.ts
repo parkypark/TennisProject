9cbfa3e74aa76964e187df9e71529be389d3ca9d
Patrick Park <parkypark86@gmail.com>
2018-06-07 14:15:13 -0700

    first one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { pageNonFoundComponent } from './pageNonFound/pageNonFound.component';
 
+
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
+const appRoutes: Routes = [
+  {path: 'main', component: IndexComponent},
+  { path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  { path: '**', component: pageNonFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    pageNonFoundComponent,
+    EditComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing:true }
+    ),
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
+  providers: []
 })
 export class AppModule { }
---
+++ src/app/pageNonFound/pageNonFound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './pageNonFound.component.html',
+  styleUrls: ['./pageNonFound.component.css']
+})
+export class pageNonFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
